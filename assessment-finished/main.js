fea613e6c00668136c6e6833a0f295205c35a576
risakoomi <144974464+risakoomi@users.noreply.github.com>
2023-10-05 22:04:51 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit at Hawaii, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Santa Claus saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const storyText = 'It was 6 fahrenheit at Hawaii, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Santa Claus saw the whole thing, but was not surprised — :insertx: weighs 8 pounds, and it was a cold day.';
 const insertX = ['Mickey', 'Minnie', 'Panda'];
 const insertY = ['Stadium', 'Library', 'Castle'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
