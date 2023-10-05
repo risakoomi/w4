4e209bcf3bb1bd30ae4ad9b556afde48e708e7be
risakoomi <144974464+risakoomi@users.noreply.github.com>
2023-10-05 22:03:01 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,9 +7,9 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit at Tsuda, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const storyText = 'It was 94 fahrenheit at Hawaii, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Santa Claus saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Mickey', 'Minnie', 'Panda'];
+const insertY = ['Stadium', 'Library', 'Castle'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
